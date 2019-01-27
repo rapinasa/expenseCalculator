b2cc915cbedc3b28d2fa2ca321a2270d2903fb3a
rapinasa <rapina1990@gmail.com>
2019-01-27 12:55:46 +0200

    modified:   file/expences.html 	modified:   file/jscript.js

--- file/jscript.js
+++ file/jscript.js
@@ -1,44 +1,30 @@
 
+	var myArr = [];
+function myFunction(){
+  var a = document.getElementById("money1").value;
 
+myArr.push(parseInt(a));
 
-
-
-
-
-
-
-
-/*
-function Calculator() {
-var a = document.getElementById("moneyVegetable1").value;
-var b = document.getElementById("moneyVegetable2").value;
-var c = document.getElementById("moneyVegetable3").value;
-var d = document.getElementById("moneyDrinks1").value;
-var e = document.getElementById("moneyDrinks2").value;
-var f = document.getElementById("moneyDrinks3").value;
-var g = document.getElementById("moneyCompayments1").value;
-var h = document.getElementById("moneyCompayments2").value;
-var j = document.getElementById("moneyCompayments3").value;
-var k = document.getElementById("moneyCars1").value;
-var l = document.getElementById("moneyCars2").value;
-var m = document.getElementById("moneyCars3").value;
-var z = Number(a) + Number(b) + Number(c);
-    document.getElementById("total").innerHTML=z;
-     document.getElementById("moneyVegetable1").value = "";
-      document.getElementById("moneyVegetable2").value = "";
-       document.getElementById("moneyVegetable3").value = "";
-   };
-
-       /*var Obj = {
-       }
-        var serialObj = JSON.stringify(z);
- localStorage.setItem("myKey", serialObj);
- var returnObj = JSON.parse(localStorage.getItem("myKey"));
-};*/
- 
-
-
-
-    /*localStorage.setItem("keys", z);
-var localValue = localStorage.getItem("keys");
-document.getElementById("total").innerHTML = localValue;*/
+var z = "";
+for (i = 0; i < myArr.length;i++){
+z = z + myArr[i]+"<br/>";
+}
+  saveArr();
+}
+function getSum(total, num){
+  return total + num;
+  saveArr();
+}
+function Counting(item){
+  document.getElementById("total1").innerHTML = myArr.reduce(getSum);
+}
+function saveArr(){
+var JSONReady = JSON.stringify(myArr);
+localStorage.setItem("myKey", JSONReady);
+}
+function getArr(){
+var JSONReady = localStorage.getItem("myKey");
+myArr = JSON.parse(JSONReady);
+}
+getArr();
+Counting();
